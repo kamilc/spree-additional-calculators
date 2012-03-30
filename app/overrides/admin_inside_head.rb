ef1f50cde2d5ee5c639340752ab592f36a744b36
Thomas de Grivel <billitch@gmail.com>
2012-03-30 05:50:02 +0200

    remove javascript tag, please use //= require admin/additional_calculators in app/assets/javascripts/admin/all.js

--- app/overrides/admin_inside_head.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "layouts/admin",
-                     :name => "admin_inside_head",
-                     :insert_after => "[data-hook='admin_inside_head'], #admin_inside_head[data-hook]",
-                     :partial => "shared/additional_calculators_admin_head",
-                     :disabled => false)
